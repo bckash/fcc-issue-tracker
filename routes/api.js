af88224a95899a2ca9894821efeba1ce7ee2e9eb
bart c <kaszikasz@wp.pl>
2023-07-06 22:10:32 +0100

    All requests passing tests

--- routes/api.js
+++ routes/api.js
@@ -11,24 +11,29 @@ module.exports = function (app) {
       let project = req.params.project;
 
       IssueArrayModel
-        .find({ name: project })
+        .findOne({ name: project })
         .then( result => {
-
-          if (result[0] !== undefined) {
-            let issueArr = result[0].issues
+          if (result !== undefined) {
+            let issueArr = result.issues
             let filters = req.query
 
             // if filters = {}, for loop is omiited
             const filteredIssue = issueArr.filter( issue => {
               for (const key in filters) {
                 if (issue[key] !== filters[key]) {
-                  return false;
+                  if (issue._id.toString() === filters._id) {
+                    return true;
+                  } else {
+                    return false
+                  }
                 }
               }
               return true
             })
 
-          res.json(filteredIssue)
+            res.json(filteredIssue)
+          } else {
+            res.json({})
           }
         })
     })
@@ -51,7 +56,7 @@ module.exports = function (app) {
 
       let newIssueArrayObj = {
         name: project,
-        issues: newIssueObj
+        issues: [newIssueObj]
       }
 
       if (!issue_title  || !issue_text || !created_by ) {
@@ -67,7 +72,6 @@ module.exports = function (app) {
                 newIssueArray
                   .save()
                   .then( savedArr => {
-                    console.log(savedArr.issues[0])
                     res.json(savedArr.issues[0])
                   })
             } else {
@@ -76,7 +80,6 @@ module.exports = function (app) {
                   .save() // <- mongoose knows ive pushed to"issues" on my document, and "save()" becomes "updateOne()"
                   .then( savedArr => {
                     let last = savedArr.issues.length - 1
-                    console.log(savedArr.issues[last])
                     res.json(savedArr.issues[last])
                   })
             }
@@ -86,13 +89,71 @@ module.exports = function (app) {
     })
     
     .put(function (req, res){
+
       let project = req.params.project;
-      
+      let { _id, issue_title, issue_text, created_by, assigned_to, status_text, open } = req.body
+
+      if (!_id) {
+        res.json({ error: 'missing _id' })
+        return
+
+      } else if (!issue_title && !issue_text && !created_by && !assigned_to && !status_text && !open) {
+        res.json({ error: 'no update field(s) sent', '_id': _id })
+        return
+
+      } else {
+        IssueArrayModel
+          .findOne({ name: project })
+          .then( result => {
+            if (!result) {
+              res.json({ error: 'could not update', '_id': _id })
+              return
+            } else {
+              let issue = result.issues.id(_id)
+              if (!issue) {
+                res.json({ error: 'could not update', '_id': _id })
+                return
+              } else {
+                issue.issue_title = issue_title || issue.issue_title
+                issue.issue_text  = issue_text  || issue.issue_text
+                issue.created_by  = created_by  || issue.created_by
+                issue.assigned_to = assigned_to || issue.assigned_to
+                issue.status_text = status_text || issue.status_text
+                issue.updated_on  = new Date()
+                issue.open = open || issue.open
+                result
+                  .save()
+                  .then(res.json({  result: 'successfully updated', '_id': _id }))
+              }
+            }
+          })
+      }
+
     })
     
     .delete(function (req, res){
       let project = req.params.project;
-      
+      let { _id } = req.body
+
+      if (!_id) {
+        res.json({ error: 'missing _id' })
+        return
+      } else {
+        IssueArrayModel
+          .findOne({ name: project })
+          .then( result => {
+            let issue = result.issues.id(_id)
+            if (!issue) {
+              res.json({ error: 'could not delete', '_id': _id })
+              return
+            } else {
+              issue.deleteOne()
+              result
+                .save()
+                .then(res.json({ result: 'successfully deleted', '_id': _id }))
+            }
+        })
+      }
     });
     
 };
